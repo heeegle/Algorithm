e8a1be4f3284845b7af3382ff8fbb242cafbe2d5
박성희 <132249005+heeegle@users.noreply.github.com>
2024-01-08 10:08:30 +0900

    Update 짝수와 홀수.java
    2가지(if문, 삼항연산자) 풀이 방식

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,6 +1,20 @@
+// if문을 이용한 풀이
 class Solution {
     public String solution(int num) {
         String answer = "";
-        return (num % 2 == 0) ? "Even" : "Odd";
+        if (num % 2 == 0) {
+            answer = "Even";
+        }else {
+            answer = "Odd";
+        }
+        return answer;
     }
 }
+
+// 삼항연산자를 이용한 풀이
+// class Solution {
+//     public String solution(int num) {
+//         String answer = "";
+//         return (num % 2 == 0) ? "Even" : "Odd";
+//     }
+// }
