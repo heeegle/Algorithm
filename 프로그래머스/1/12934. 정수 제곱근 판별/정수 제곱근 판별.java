e8112dec335aecfdfc45f19b403b1171e15acdc6
박성희 <132249005+heeegle@users.noreply.github.com>
2024-01-11 09:50:24 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,13 +1,14 @@
 class Solution {
-    public long solution(long n) {
-        long answer = -1; // 기본적으로 -1로 초기화
+  public long solution(long n) {
+      long answer = 0;
 
-        for (long i = 1; i * i <= n; i++) {
-            if (i * i == n) {
-                answer = (i + 1) * (i + 1);
-                break; // 원하는 조건을 만족하면 반복문을 종료
-            }
-        }
-        return answer;
-    }
+      for (long i = 1; i <= n; i++) {
+          if (i * i == n) {
+              answer = (i + 1) * (i + 1);
+              break;
+          }
+          else answer = -1;
+      }
+      return answer;
+  }
 }
